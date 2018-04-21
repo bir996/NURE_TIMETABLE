d83b328080a6fb6dee95140c9144949074deef03
Maxim Biriukov <maxim.biriukov@vilmate.com>
2018-04-21 21:48:01 +0300

    Initial commit

---
+++ TimeTable/NURE_Time_Table/TodayViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TodayViewController.swift
+//  NURE_Time_Table
+//
+//  Created by Maxim Biriukov on 4/21/18.
+//  Copyright © 2018 Maksym Biriukov. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
